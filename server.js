6e148568a03a4d89da11707eeecd1ad09d0e0a59
Akshay Waghmare <akshaywaghmare@Akshays-MacBook-Air.local>
2024-02-21 04:59:49 +0530

    error-fiex

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyparser = require("body-parser");
 const fs =require("fs");
-const https = require('https');
+const http = require('http');
 
 
 const app = express();
